4b2b900cc9744a23510edd77d754a76c81765c6c
daniilBarabanov <55635367+daniilBarabanov@users.noreply.github.com>
2020-09-27 23:33:21 +0300

    nav

--- build/js/menu-script.js
+++ build/js/menu-script.js
@@ -8,13 +8,19 @@
 } */
 function myFunction() {
     var x = document.getElementById("myLinks");
-    var y = document.getElementById("uu");
+    var y = document.getElementById("header-top");
+    var z = document.getElementById("page-header__top-bottom");
     if (x.style.display === "block") {
       x.style.display = "none";
       y.style.background = "rgba(0,0,0,.3)";
+      z.style.background = "none"
     } else {
       x.style.display = "block";
       y.style.background = "#283645 ";
+      z.style.background = "url(../img/back-mobile-1-x-jpg.png)"
+      z.style.backgroundPosition = "0"
+      z.style.backgroundRepeat = "no-repeat"
+      z.style.backgroundSize = "cover"
     }
   }
-  console.log("adadada");
+  console.log("qwe");
